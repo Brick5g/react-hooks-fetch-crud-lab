f684c3a6c779159b973cd2c1777e23cb7af6001a
Julian Francis <jgfrancis3@gmail.com>
2026-06-16 09:56:50 -0400

    7.10 lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,35 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => onDeleteQuestion(id));
+  }
+
+function handleDropdownChange(event) {
+  const newIndex = parseInt(event.target.value);
+
+  onUpdateQuestion({ ...question, correctIndex: newIndex });
+
+  fetch(`http://localhost:4000/questions/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      correctIndex: newIndex,
+    }),
+  })
+    .then((res) => res.json())
+    .then((updatedQuestion) => {
+      onUpdateQuestion(updatedQuestion); 
+    });
+}
 
   return (
     <li>
@@ -15,9 +37,15 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select value={correctIndex} onChange={handleDropdownChange}>
+          {answers.map((answer, index) => (
+            <option key={index} value={index}>
+              {answer}
+            </option>
+          ))}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
